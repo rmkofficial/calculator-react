18f57c9a32a79e6a3a693be5500b638b704995c3
Ramazan Mert Kaya <kmert0@outlook.com>
2023-05-21 20:19:04 +0300

    Adding useState

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,28 +1,64 @@
+import { useState } from "react";
 import "./Calculator.css";
 
 const Calculator = () => {
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [result, setResult] = useState(0);
+
+  const handleNumber1Change = (e) => {
+    setNumber1(Number(e.target.value));
+  };
+
+  const handleNumber2Change = (e) => {
+    setNumber2(Number(e.target.value));
+  };
+
+  const handleAddition = () => {
+    setResult(number1 + number2);
+  };
+
   return (
     <div className="calculator-container">
       <h1>useState</h1>
       <div className="form-container">
         <div className="form-group">
-          <label htmlFor="number1" className="form-label">
-            Number 1
+          <label className="form-label" htmlFor="number1">
+            Number1
           </label>
-          <input type="number" className="form-input" id="number2"></input>
+          <input
+            className="form-input"
+            type="number"
+            id="number1"
+            onChange={handleNumber1Change}
+            value={number1}
+          />
         </div>
         <div className="form-group">
-          <label htmlFor="number2" className="form-label">
-            Number 2
+          <label className="form-label" htmlFor="number2">
+            Number2
           </label>
-          <input type="number" className="form-input" id="number2"></input>
+          <input
+            className="form-input"
+            type="number"
+            id="number2"
+            onChange={handleNumber2Change}
+            value={number2}
+          />
         </div>
-        <button className="form-button">Add</button>
+        <button className="form-button" onClick={handleAddition}>
+          Add
+        </button>
         <div className="form-group">
-          <label htmlFor="result" className="form-label">
+          <label className="form-label" htmlFor="result">
             Result
           </label>
-          <input type="number" className="form-input" id="result"></input>
+          <input
+            className="form-input"
+            type="number"
+            id="result"
+            value={result}
+          />
         </div>
       </div>
     </div>
